8bc112378534f288aa21380fbe023781e79a9805
OscarRoman1230 <48228133+oscarroman1230@users.noreply.github.com>
2020-07-25 01:27:32 -0500

    Yupiiii x6

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+'/dist/front-pruebas'));
+app.use(express.static(__dirname+'/dist/front'));
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/front-pruebas/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/front/index.html'));
 });
 app.listen(process.env.PORT || 8080);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('front/build'));
+  app.use(express.static('front-p/build'));
 }
 
 app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'front/build', 'index.html'));
+  response.sendFile(path.join(__dirname, 'front-p/build', 'index.html'));
 });
