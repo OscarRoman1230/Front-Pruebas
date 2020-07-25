fb2350f501dd1fe7392e1079ffab42f41fa71bfc
OscarRoman1230 <48228133+oscarroman1230@users.noreply.github.com>
2020-07-25 01:03:08 -0500

    Yupiiii x4

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+'/dist/sci-commerce-app'));
+app.use(express.static(__dirname+'/dist/front-pruebas'));
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/sci-commerce-app/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/front-pruebas/index.html'));
 });
 app.listen(process.env.PORT || 8080);
 
