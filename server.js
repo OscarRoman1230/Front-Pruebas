36e5b35a753d9fcfb197645a5dd691f07ad946ce
OscarRoman1230 <48228133+oscarroman1230@users.noreply.github.com>
2020-07-25 21:15:31 -0500

    Yupiiii x11

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+'/dist/front'));
+app.use(express.static(__dirname+'/dist/Front'));
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/front/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Front/index.html'));
 });
 app.listen(process.env.PORT || 8080);
 
